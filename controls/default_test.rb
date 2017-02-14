6712dfdefb11472fbe6c98ee354430dc1c276d33
Annie Hedgpeth <annie@Annies-MBP.tx.rr.com>
2017-02-14 14:43:31 -0600

    initial commit

---
+++ controls/default_test.rb
@@ -0,0 +1,39 @@
+describe package('nano') do
+  it { should be_installed }
+end
+
+describe directory('/var/website') do
+  it { should exist }
+end
+
+describe directory('/var/old-website') do
+  it { should_not exist }
+end
+
+describe file('/var/website/directions.txt') do
+  it { should exist }
+  its('content') { should eq 'website goes here' }
+end
+
+describe file('/var/website/builder.txt') do
+  it { should exist }
+  its('content') { should match 'Test Kitchen built this' }
+end
+
+describe file('/var/website/logo.jpg') do
+  it { should exist }
+end
+
+describe file('/var/website/command.txt') do
+  it { should exist }
+  its('content') { should match 'ran command' }
+end
+
+describe directory('/var/website/arch') do
+  it { should exist }
+end
+
+describe file('/var/website/welcome.txt') do
+  it { should exist }
+  its('content') { should eq 'Welcome To Chef Learning!' }
+end
